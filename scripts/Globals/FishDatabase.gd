773b40d61263ad56a1fa9068474c3e81cc31df3d
João Araujo <juniormedeirosales@gmail.com>
2025-07-18 19:37:37 -0300

    changed forest map
    +finished prototype map zone
    +added more fishes

--- scripts/Globals/FishDatabase.gd
+++ scripts/Globals/FishDatabase.gd
@@ -17,7 +17,7 @@ const DATA = {
 	},
 	"TRUTA": {
 		"name": "Truta",
-		"raridade": "Normal ★", 
+		"raridade": "Incomum ★★", 
 		"base_price": 20,
 		"spawn_weight": 60,
 		"sprite": preload("res://assets/Sprites/fishs/TRUTA.png"),
@@ -27,9 +27,33 @@ const DATA = {
 		"name": "Athanasios",
 		"raridade": "Histórico ★★★★★★", 
 		"base_price": 999,
-		"spawn_weight": 10,
+		"spawn_weight": 5,
 		"sprite": preload("res://assets/Sprites/fishs/thanos.png"),
 		"description": "Esse peixe dá aula de fundamentos de matemática para a computação e ainda joga basquete."
+	},
+	"PEIXINHO_DOURADO": {
+		"name": "Peixinho Dourado",
+		"raridade": "Raro ★★★", 
+		"base_price": 45,
+		"spawn_weight": 25,
+		"sprite": preload("res://assets/Sprites/fishs/goldfish.png"),
+		"description": "um classico animal de estimação que estrelou varios filmes de sucesso"
+	},
+	"BAIACU": {
+		"name": "BaiaCu doce",
+		"raridade": "Épico ★★★★", 
+		"base_price": 53,
+		"spawn_weight": 16,
+		"sprite": preload("res://assets/Sprites/fishs/pufferfish.png"),
+		"description": "quem diria que existiria Baicu aqui, nesse lago tem de tudo"
+	},
+	"MISING": {
+		"name": "redacted",
+		"raridade": "Lendário ★★★★", 
+		"base_price": 120,
+		"spawn_weight": 7,
+		"sprite": preload("res://assets/Sprites/fishs/missingfish.png"),
+		"description": "oq é isso??!!??"
 	}
 }
 
